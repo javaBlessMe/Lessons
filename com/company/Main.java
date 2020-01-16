b7cbfc5253ebdd820a210e3368b08e82ea1bdb6b
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2020-01-16 23:13:32 +0300

    'final'

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        leapYear(1900);
     }
     private static int firstFunction (int a, int b, int c, int d){
         return a * (b + (c / d));
@@ -15,5 +15,24 @@ public class Main {
     private static void thirdFunction(int a){
         if(a>=0) System.out.println("Передано положительно число");
         else System.out.println("Передали отрицательное число");
+    }
+    private static boolean fourth(int a){
+       return a<0;
+    }
+    private static void fives(String str){
+        System.out.println("Привет, "+str);;
+    }
+    private static void leapYear(int year){
+        if(year%4!=0){
+            System.out.println("Это невисокосный год");
+        }
+        else
+        {
+            if(year%100==0 && year%400!=0){
+                System.out.println("Это невисокосный год");
+            }
+            else System.out.println("Это високосный год");
+
+        }
     }
 }
